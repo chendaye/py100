f0d1c85965a018e32df0501c63366cd67888acf4
lengo <chendaye666@gmail.com>
2019-10-18 13:49:36 +0800

    py100

---
+++ 05.py
@@ -0,0 +1,14 @@
+"""
+完美数(自身以外的因子)
+6=1+2+3
+28=1+2+4+7+14
+"""
+
+for num in range(1, 10000):
+    sum = 0
+    tmp = num
+    for m in range(1, tmp - 1):
+        if tmp % m == 0:
+            sum += m
+    if sum == num:
+        print(num)
