58d893d57d5199c8db14e2e20edef00cf4e7fc27
lengo <chendaye666@gmail.com>
2019-10-30 15:45:36 +0800

    进程

--- 协程.py
+++ 协程.py
@@ -1,6 +1,10 @@
 import time
 import tkinter
 import tkinter.messagebox
+from threading import Thread
+from time import time
+from multiprocessing import Process, Queue
+from random import randint
 
 
 def download():
@@ -12,7 +16,8 @@ def download():
 def show_about():
     tkinter.messagebox.showinfo('关于', '作者: 骆昊(v1.0)')
 
-
+# 用休眠的方式模拟点击“下载”按钮会联网下载文件需要耗费10秒的时间，如果不使用“多线程”，
+# 我们会发现，当点击“下载”按钮后整个程序的其他部分都被这个耗时间的任务阻塞而无法执行了
 def main():
     top = tkinter.Tk()
     top.title('单线程')
@@ -29,5 +34,95 @@ def main():
     tkinter.mainloop()
 
 
+
+# 用多线程将耗时间的任务放到一个独立的线程中执行，这样就不会因为执行耗时间的任务而阻塞了主线程
+def main1():
+
+    # 继承Thread，另起一个线程
+    class DownloadTaskHandler(Thread):
+
+        def run(self):
+            time.sleep(10)
+            tkinter.messagebox.showinfo('提示', '下载完成!')
+            # 启用下载按钮
+            button1.config(state=tkinter.NORMAL)
+
+    def download():
+        # 禁用下载按钮
+        button1.config(state=tkinter.DISABLED)
+        # 通过daemon参数将线程设置为守护线程(主程序退出就不再保留执行)
+        # 在另一个线程中处理耗时间的下载任务
+        DownloadTaskHandler(daemon=True).start()
+
+    def show_about():
+        tkinter.messagebox.showinfo('关于', '作者: 骆昊(v1.0)')
+
+    top = tkinter.Tk()
+    top.title('单线程')
+    top.geometry('200x150')
+    top.wm_attributes('-topmost', 1)
+
+    panel = tkinter.Frame(top)
+    button1 = tkinter.Button(panel, text='下载', command=download)
+    button1.pack(side='left')
+    button2 = tkinter.Button(panel, text='关于', command=show_about)
+    button2.pack(side='right')
+    panel.pack(side='bottom')
+
+    tkinter.mainloop()
+
+
+# 计算密集型任务
+def main2():
+    total = 0
+    number_list = [x for x in range(1, 100000001)]
+    start = time()
+    for number in number_list:
+        total += number
+    print(total)
+    end = time()
+    print('Execution time: %.3fs' % (end - start))
+
+
+# 将这个任务分解到8个进程中去执行
+def task_handler(curr_list, result_queue):
+    total = 0
+    for number in curr_list:
+        total += number
+    # 计算结果放入队列中
+    result_queue.put(total)
+
+
+def main3():
+    processes = []
+    number_list = [x for x in range(1, 100000001)]
+    # 生成一个队列
+    result_queue = Queue()
+    index = 0
+    # 启动8个进程将数据切片后进行运算
+    for _ in range(8):
+        # 生成一个进程
+        p = Process(target=task_handler,
+                    args=(number_list[index:index + 12500000], result_queue))
+        # 12500000 * 8
+        index += 12500000
+        processes.append(p)
+        # 启动进程
+        p.start()
+    # 开始记录所有进程执行完成花费的时间
+    start = time()
+    for p in processes:
+        # 结束进程
+        p.join()
+    # 合并执行结果
+    total = 0
+    while not result_queue.empty():
+        total += result_queue.get()
+    print(total)
+    end = time()
+    print('Execution time: ', (end - start), 's', sep='')
+
+
+
 if __name__ == '__main__':
-    main()
+    main3()
