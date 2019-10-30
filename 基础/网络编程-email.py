e44c7c63df4ae91431a16e5fec1e05ced6764f80
chendaye <chendaye666@gmail.com>
2019-10-30 21:37:52 +0800

    基础

--- 基础/网络编程-email.py
+++ 基础/网络编程-email.py
@@ -1,21 +1,82 @@
 from smtplib import SMTP
 from email.header import Header
 from email.mime.text import MIMEText
+from email.mime.image import MIMEImage
+from email.mime.multipart import MIMEMultipart
+import urllib
 
 
 def main():
     # 请自行修改下面的邮件发送者和接收者
-    sender = 'abcdefg@126.com'
-    receivers = ['uvwxyz@qq.com', 'uvwxyz@126.com']
+    sender = '834261743@qq.com'
+    receivers = ['15271834241@163.com']
     message = MIMEText('用Python发送邮件的示例代码.', 'plain', 'utf-8')
-    message['From'] = Header('王大锤', 'utf-8')
-    message['To'] = Header('骆昊', 'utf-8')
+    message['From'] = Header('chendaye', 'utf-8')
+    message['To'] = Header('lengo', 'utf-8')
     message['Subject'] = Header('示例代码实验邮件', 'utf-8')
     smtper = SMTP('smtp.126.com')
     # 请自行修改下面的登录口令
-    smtper.login(sender, 'secretpass')
+    smtper.login(sender, 'lengo@long666')
     smtper.sendmail(sender, receivers, message.as_string())
     print('邮件发送完成!')
+
+# 带附件的邮件
+def main2():
+    # 创建一个带附件的邮件消息对象
+    message = MIMEMultipart()
+
+    # 创建文本内容
+    text_content = MIMEText('附件中有本月数据请查收', 'plain', 'utf-8')
+    message['Subject'] = Header('本月数据', 'utf-8')
+    # 将文本内容添加到邮件消息对象中
+    message.attach(text_content)
+
+    # 读取文件并将文件作为附件添加到邮件消息对象中
+    with open('/Users/Hao/Desktop/hello.txt', 'rb') as f:
+        txt = MIMEText(f.read(), 'base64', 'utf-8')
+        txt['Content-Type'] = 'text/plain'
+        txt['Content-Disposition'] = 'attachment; filename=hello.txt'
+        message.attach(txt)
+
+
+    # 创建SMTP对象
+    smtper = SMTP('smtp.126.com')
+    # 开启安全连接
+    # smtper.starttls()
+    sender = 'abcdefg@126.com'
+    receivers = ['uvwxyz@qq.com']
+    # 登录到SMTP服务器
+    # 请注意此处不是使用密码而是邮件客户端授权码进行登录
+    # 对此有疑问的读者可以联系自己使用的邮件服务器客服
+    smtper.login(sender, 'secretpass')
+    # 发送邮件
+    smtper.sendmail(sender, receivers, message.as_string())
+    # 与邮件服务器断开连接
+    smtper.quit()
+    print('发送完成!')
+
+
+# 短信
+import urllib.parse
+import http.client
+import json
+
+
+def main3():
+    host  = "106.ihuyi.com"
+    sms_send_uri = "/webservice/sms.php?method=Submit"
+    # 下面的参数需要填入自己注册的账号和对应的密码
+    params = urllib.parse.urlencode({'account': '你自己的账号', 'password' : '你自己的密码', 'content': '您的验证码是：147258。请不要把验证码泄露给其他人。', 'mobile': '接收者的手机号', 'format':'json' })
+    print(params)
+    headers = {'Content-type': 'application/x-www-form-urlencoded', 'Accept': 'text/plain'}
+    conn = http.client.HTTPConnection(host, port=80, timeout=30)
+    conn.request('POST', sms_send_uri, params, headers)
+    response = conn.getresponse()
+    response_str = response.read()
+    jsonstr = response_str.decode('utf-8')
+    print(json.loads(jsonstr))
+    conn.close()
+
 
 
 if __name__ == '__main__':
